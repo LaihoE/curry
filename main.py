a6d2aa8abae3cc3463e65d799dfe1fb47a3e8faa
PythonBoost <emillaiho@hotmail.fi>
2021-09-10 19:00:12 +0300

    Added bass boosting

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 from discord import FFmpegPCMAudio
+import youtube_dl
 from youtube_dl import YoutubeDL
 from asyncio import sleep
 import requests
 import pandas as pd
+from bass_boost import booster
+import os
 
 def get_passwords():
     df = pd.read_csv(r"C:\Users\emill\Desktop/secrets.csv")
@@ -26,7 +29,6 @@ def get_url(videoname,api_key):
 def start():
     api_key,token,channel = get_passwords()
     client = commands.Bot(command_prefix='.')
-    total = 0
 
 
     @client.event
@@ -37,6 +39,7 @@ def start():
                 if vc.guild == message.guild:
                     await vc.disconnect()
 
+        # Normal play
         if "-p" in message.content:
             print(message.content)
             vca = client.get_channel(channel)
@@ -49,6 +52,34 @@ def start():
                 info = ydl.extract_info(url, download=False)
             URL = info['formats'][0]['url']
             vc.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+            while vc.is_playing():
+                await sleep(1)
+
+        # Bass boosted
+        if "-bp" in message.content:
+            vca = client.get_channel(channel)
+            vc = await vca.connect()
+            query = message.content.replace("-bp", "")
+            query = query.split("boost")[0]
+
+            url = get_url(query, api_key)
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': 'C:/Users/emill/PycharmProjects/curry/mp3/%(title)s-%(id)s.%(ext)s',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }]
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            if "boost=" in message.content:
+                db= message.content.split("=")[1]
+                booster(int(db),int(db))
+            files = os.listdir(r"C:\Users\emill\PycharmProjects\curry\bass_boosted")
+
+            vc.play(FFmpegPCMAudio(f'C:/Users/emill/PycharmProjects/curry/bass_boosted/{files[0]}'))
             while vc.is_playing():
                 await sleep(1)
 
