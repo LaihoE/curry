fdb838f4d9dcdb719099d6bcaa8103b6b11b8d88
Markus <markus.kirjonen@hotmail.com>
2021-09-13 17:46:30 +0300

    own version

--- main.py
+++ main.py
@@ -2,55 +2,45 @@ from discord.ext import commands
 from discord import FFmpegPCMAudio
 from youtube_dl import YoutubeDL
 from asyncio import sleep
+import requests
+import pandas as pd
+
+def get_passwords():
+    df = pd.read_csv(r"C:\Users\emill\Desktop/secrets.csv")
+    api_key = df["apikey"][0]
+    token = df["token"][0]
+    channel = df["channel"][0]
+    return api_key,token,channel
 
 
-def download_video(videoname):
+def get_url(videoname,api_key):
     videoname.replace(' ','&')
-    apikey=''
-    x=requests.get(f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={videoname}&key={apikey}").json()
-    print(f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={videoname}&key={apikey}")
+    x=requests.get(f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={videoname}&key={api_key}").json()
+    print(f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={videoname}&key={api_key}")
     vidid=x['items'][0]['id']['videoId']
     url='https://www.youtube.com/watch?v='+ vidid
     print(url)
     return url
 
 
-def instaspeak():
-    token = ''
+def start():
+    api_key,token,channel = get_passwords()
     client = commands.Bot(command_prefix='.')
     total = 0
-    @client.event
-    async def on_ready():
-        vca = client.get_channel()
-        vc = await vca.connect()
 
     @client.event
     async def on_message(message):
-        # CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-        if message.content == "cow sleeping":
-            await message.channel.send("sory slep well")
-            for vc in client.voice_clients:
-                if vc.guild == message.guild:
-                    await vc.disconnect()
-
         if "-stop" in message.content:
             print(message.content)
             for vc in client.voice_clients:
                 if vc.guild == message.guild:
                     await vc.disconnect()
 
-
         if "-p" in message.content:
             print(message.content)
-            for vc in client.voice_clients:
-                if vc.guild == message.guild:
-                    await vc.disconnect()
-
-            if total ==0:
-                vca = client.get_channel()
-                vc = await vca.connect()
-
-            url = download_video(message.content.replace("-p",""))
+            vca = client.get_channel(channel)
+            vc = await vca.connect()
+            url = get_url(message.content.replace("-p",""),api_key)
             YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
             FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
                               'options': '-vn'}
@@ -61,7 +51,6 @@ def instaspeak():
             while vc.is_playing():
                 await sleep(1)
 
-        
     client.run(token)
 
-instaspeak()
+start()
